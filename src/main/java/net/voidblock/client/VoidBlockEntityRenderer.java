624f3b13a09d64ce06f7cf9407e4c1dcaa0f4474
Joel Taylor <joel.w.taylor@gmail.com>
2025-08-21 16:51:43 +0100

    changed to endGateway

--- src/main/java/net/voidblock/client/VoidBlockEntityRenderer.java
+++ src/main/java/net/voidblock/client/VoidBlockEntityRenderer.java
@@ -26,7 +26,7 @@ public class VoidBlockEntityRenderer implements BlockEntityRenderer<VoidBlockEnt
         if (!entity.getBlockState().getValue(VoidBlock.ACTIVE)) return;
 
         // Try endGateway first for better shader compatibility
-        VertexConsumer vb = buffers.getBuffer(RenderType.endPortal());
+        VertexConsumer vb = buffers.getBuffer(RenderType.endGateway());
         renderCube(poseStack, vb, light, overlay);
     }
 
